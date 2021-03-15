d6f33c275db016ff0aad5eb12a4f1297fa1f6728
Aiden-NSI <80536000+Aiden-NSI@users.noreply.github.com>
2021-03-15 15:53:57 +0100

    Update Chess.py

--- Chess.py
+++ Chess.py
@@ -1,1 +1,11 @@
 #ici nous allons code le jeu d'échec
+# ------------- INITIALISATION-------------------
+import pygame
+import sys
+
+pygame.init()  # essential for pygame
+pygame.font.init()  # text
+
+# DEFINITION DE LA FENETRE
+screen = pygame.display.set_mode((800, 60 * 8))
+pygame.display.set_caption('ChessNSI')
