2069756bbdd6214ca78a77aa46583c308c5cac09
Aiden-NSI <80536000+Aiden-NSI@users.noreply.github.com>
2021-03-26 15:37:04 +0100

    Update __init__.py

--- menus/__init__.py
+++ menus/__init__.py
@@ -1,6 +1,2 @@
-'''
-This file is a part of My-PyChess application.
-In this file, we import a few modules for easy access for main file
-'''
-
 from menus.about import main as aboutmenu
+from menus.pref import main as prefmenu
