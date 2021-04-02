ca59ef280ec72b0f3da90a420b2dfed985c8d482
Courtepaille <76945267+Courtepaille@users.noreply.github.com>
2021-04-02 15:46:09 +0200

    Add files via upload

---
+++ piece.py
@@ -0,0 +1,17 @@
+﻿class Piece:
+    VIDE='.'
+    nomPiece=(VIDE,'ROI','DAME','TOUR','CAVALIER','FOU','PION')
+    color=(VIDE,'BLANC','NOIR')
+    valeurPiece=(0,0,9,5,3,3,1)
+    deplacements_tour=(-10,10,-1,1)
+    deplacements_fou=(-11,-9,11,9)
+    deplacements_cavalier=(-12,-21,-19,-8,12,21,19,8)
+    def __init__(self,nomPiece,couleur):
+        self.nomPiece = nomPiece
+        self.couleur = color
+        self.valeur = valeurPiece
+
+
+tour = Piece('TOUR','NOIR')
+
+
