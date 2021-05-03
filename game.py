f0f192e9d296e868fdc1a7da6dd4289a635fe387
Aiden-NSI <80536000+Aiden-NSI@users.noreply.github.com>
2021-05-03 11:00:24 +0200

    Update game.py

--- game.py
+++ game.py
@@ -1,6 +1,4 @@
 import pygame
-from tour import Tour
-from plateau import Plateau
 
 #Création d'une classe Game
 class Game:
@@ -10,17 +8,3 @@ class Game:
         #définir si le jeu a commencé ou non
         self.is_playing = False
 
-        #générer notre plateau
-        self.plateau = Plateau()
-
-        #générer notre Tour
-        self.tour = Tour()
-
-
-
-
-    def update(self, screen):
-        #appliquer l'image de mon plateau
-        screen.blit(self.plateau.image, self.plateau.rect)
-        # appliquer l'image de mon joueur
-        screen.blit(self.tour.image, self.tour.rect)
