235c71d9eedda3c32d99236ea04cb431e96e1a87
Aiden-NSI <80536000+Aiden-NSI@users.noreply.github.com>
2021-05-06 21:34:37 +0200

    Update jeu.py

--- jeu.py
+++ jeu.py
@@ -2,9 +2,30 @@
 import pygame, sys, os, math
 sys.dont_write_bytecode = True
 from piecelogique import *
-from imagedefs import *
 
 pygame.init()
+
+#-------IMAGES-----------
+highlightimg = pygame.image.load("res/pieces/highlight.png")
+hhoverimg = pygame.image.load("res/pieces/hhover.png")
+attackimg = pygame.image.load("res/pieces/attack.png")
+icon = pygame.image.load('res/chess_icon.jpg')
+
+
+pieceimages = {}
+pieceimages["WPawn"] = pygame.image.load("res/pieces/wpawn.png")
+pieceimages["WRook"] = pygame.image.load("res/pieces/wrook.png")
+pieceimages["WKnight"] = pygame.image.load("res/pieces/wknight.png")
+pieceimages["WBishop"] = pygame.image.load("res/pieces/wbishop.png")
+pieceimages["WQueen"] = pygame.image.load("res/pieces/wqueen.png")
+pieceimages["WKing"] = pygame.image.load("res/pieces/wking.png")
+
+pieceimages["BPawn"] = pygame.image.load("res/pieces/bpawn.png")
+pieceimages["BRook"] = pygame.image.load("res/pieces/brook.png")
+pieceimages["BKnight"] = pygame.image.load("res/pieces/bknight.png")
+pieceimages["BBishop"] = pygame.image.load("res/pieces/bbishop.png")
+pieceimages["BQueen"] = pygame.image.load("res/pieces/bqueen.png")
+pieceimages["BKing"] = pygame.image.load("res/pieces/bking.png")
 
 #-----VARIABLES------
 tilew, tileh = 45, 45
